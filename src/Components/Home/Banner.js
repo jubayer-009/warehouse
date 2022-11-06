1d4254f2def6f6a53f53831048f196895300bc45
unknown <013jubayer@gmail.com>
2022-11-06 21:39:37 +0600

    banner

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -1,29 +1,53 @@
 import React from 'react';
-import img2 from '../../images/slide2.jpg';
-import img3 from '../../images/slide3.jpg';
-import img4 from '../../images/slide4.jpg';
+import img1 from '../../images/slide1.png';
+import img2 from '../../images/slide2.png';
+import img3 from '../../images/slide3.png';
+import img4 from '../../images/slide4.png';
 
 const Banner = () => {
-  return (<div>
-   <div className="carousel w-full h-1/2">
- 
-  <div id="item2" className="carousel-item w-full ">
-    <img src={img2} className="w-full h-1/2" />
-  </div> 
-  <div id="item3" className="carousel-item w-full">
-    <img src={img3} className="w-full h-1/2" />
-  </div> 
-  <div id="item4" className="carousel-item w-full">
-    <img src={img4} className="w-full h-1/2" />
-  </div>
-</div> 
-<div className="flex justify-center w-full  gap-2">
-  <a href="#item1" className="btn btn-xs">1</a> 
-  <a href="#item2" className="btn btn-xs">2</a> 
-  <a href="#item3" className="btn btn-xs">3</a> 
-  <a href="#item4" className="btn btn-xs">4</a>
-</div>
-</div>
+  return (
+    <div>
+      <div className="carousel w-full  mix-blend-multiply bg-primary">
+        <div id="item1" className="carousel-item w-full ">
+          <img src={img1} className="w-full " />
+        </div>
+        <div id="item2" className="carousel-item w-full ">
+          <img src={img2} className="w-full " />
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          <img src={img3} className="w-full " />
+        </div>
+        <div id="item4" className="carousel-item w-full">
+          <img src={img4} className="w-full" />
+        </div>
+      </div>
+      <div className="flex justify-center w-full py-6 gap-2">
+        <a
+          href="#item1"
+          className="btn btn-sm btn-primary text-white hover:bg-base-300 hover:text-primary"
+        >
+          1
+        </a>
+        <a
+          href="#item2"
+          className="btn btn-sm btn-primary text-white hover:bg-base-300 hover:text-primary"
+        >
+          2
+        </a>
+        <a
+          href="#item3"
+          className="btn btn-sm btn-primary text-white hover:bg-base-300 hover:text-primary"
+        >
+          3
+        </a>
+        <a
+          href="#item4"
+          className="btn btn-sm btn-primary text-white hover:bg-base-300 hover:text-primary"
+        >
+          4
+        </a>
+      </div>
+    </div>
   );
 };
 
