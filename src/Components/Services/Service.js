039ad784288d039bb569c0f26dffa5c5f56511bd
unknown <013jubayer@gmail.com>
2022-11-07 13:56:18 +0600

    service and banner section styled

--- src/Components/Services/Service.js
+++ src/Components/Services/Service.js
@@ -1,16 +1,20 @@
 import React from "react";
+import './Services.css';
 
 const Service = ({ service }) => {
   const { name, picture, about } = service;
   return (
-    <div className="p-7">
-      <div className="flex border">
-        <div>
-          <img 
-          className="bg-base-200 mr-7 h-20 w-20git"
-          src={picture} alt="" />
+    <div className="p-7  ">
+      <div className=" border rounded-lg lg:flex items-center justify-center">
+        <div className="p-7  bg-secondary  rounded-lg ">
+          <img
+            className=" lg:flex-none w-28 h-28 p-5 icon-color "
+            src={picture}
+            alt=""
+          />
         </div>
-        <div>
+
+        <div className="lg:flex-1 p-7 rounded-lg ">
           <h5 className="font-bold text-lg ">{name}</h5>
           <p>{about}</p>
         </div>
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -13,7 +13,7 @@ const Services = () => {
     <div className=" my-9 mx-9 p-10">
       <h1 className="text-5xl font-bold text-center ">Our Services</h1>
       <div className="divider mx-auto w-1/3 "></div>
-      <div className="card lg:card-side bg-base-100 shadow-xl grid grid-cols-2 gap-4  ">
+      <div className="card lg:card-side bg-base-100 shadow-xl grid  lg:grid-cols-2 grid-cols-1 gap-4  ">
         {services.map((service) => (
           <Service service={service}></Service>
         ))}
