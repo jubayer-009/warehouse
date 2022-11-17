2d746db07ecd7ad06563e598497948fd477d5127
unknown <013jubayer@gmail.com>
2022-11-17 10:32:14 +0600

    starting cart functionality ui

--- src/Components/Home/Products/ProductDetails.js
+++ src/Components/Home/Products/ProductDetails.js
@@ -81,29 +81,28 @@ const ProductDetails = () => {
               {productDetails.name}
             </h2>
             <label htmlFor="my-modal-3" className="btn">
-             
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6  right-11"
-                >
-                  <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
-                  <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
-                </svg>
-              
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6  right-11"
+              >
+                <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+                <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+              </svg>
             </label>
           </div>
 
-          <p className="w-96">{productDetails.about}</p>
-          <p>
+          <span className="w-96">{productDetails.about}</span>
+          <div>
             <span className="font-bold text-primary">Price:</span>
             {productDetails.price}
-          </p>
-          <p>
+          </div>
+          <div>
             <span className="font-bold text-primary">In Stock:</span>
             {productDetails.quantity}
-          </p>
+          </div>
+
           <div className="card-actions justify-between w-96">
             <button
               onClick={() => handleQuantity(quantity, id)}
@@ -153,9 +152,7 @@ const ProductDetails = () => {
             </div>
           </div>
         </div>
-        <ProductEditForm
-        id={id}
-        ></ProductEditForm>
+        <ProductEditForm id={id}></ProductEditForm>
       </div>
     </div>
   );
